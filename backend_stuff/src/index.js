dc0cdb8f70682bc916d170c0e57e04e5e59522e6
DaVoodooMan <83511239+DaVoodooMan@users.noreply.github.com>
2023-05-20 18:43:51 -0400

    connction test
    test connections

--- backend_stuff/src/index.js
+++ backend_stuff/src/index.js
@@ -5,9 +5,12 @@ const path  = require("path")
 const app = express();
 const server = require("http").createServer(app);
 
+
+
 const io = require("socket.io")(server);
 
 io.on("connection", function(socket){
+    console.log("it works nerd")
     socket.on("newuser", function(username){
         socket.broadcast.emit("update", username + "joined the conversation");
     });
@@ -15,4 +18,6 @@ io.on("connection", function(socket){
 
 
 
-server.listen(8000);
+server.listen(8000, function(){
+    console.log("its still working")
+});
